51ade3243a34f653dfa763c68d540fee0c50a161
mononerv <pratchaya.k.git@gmail.com>
2020-11-14 17:13:35 +0100

    Added prevent negative max guest

--- Assignments/A02/src/main/java/Controller/App.java
+++ Assignments/A02/src/main/java/Controller/App.java
@@ -25,9 +25,12 @@ public class App {
 
             try {
                 maxGuests = Integer.parseInt(tmp);
+                if (maxGuests < 0)
+                    throw new Exception("You've entered negative number");
             } catch (Exception e) {
                 maxGuests = DEFAULT_MAXGUESTS;
-                JOptionPane.showMessageDialog(null, "No input. The default will be set to " + DEFAULT_MAXGUESTS);
+                JOptionPane.showMessageDialog(null,
+                        "Invalid or no input. The default will be set to " + DEFAULT_MAXGUESTS);
             }
 
         }
