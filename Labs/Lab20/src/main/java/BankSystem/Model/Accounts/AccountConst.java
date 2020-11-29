1b764454328533530ef1ec0d18d4f76ff6da8d59
mononerv <pratchaya.k.git@gmail.com>
2020-11-29 08:20:38 +0100

    Added constanst for Lab20

--- Labs/Lab20/src/main/java/BankSystem/Model/Accounts/AccountConst.java
+++ Labs/Lab20/src/main/java/BankSystem/Model/Accounts/AccountConst.java
@@ -1,14 +1,15 @@
 package BankSystem.Model.Accounts;
 
 public interface AccountConst {
-  final long DECIMAL_OFFSET = 1000;
+  static final long DECIMAL_OFFSET = 1000;
 
-  final long MIN_AGE = 0;
+  static final long PENSION_MIN_BALANCE = 0;
+  static final long PENSION_MIN_AGE = 0;
 
-  final long SAVING_INTERESTS = 1000;
-  final long SAVING_MIN = 0;
+  static final long SAVING_INTERESTS = 1000;
+  static final long SAVING_MIN = 0;
 
-  final long CREDIT_INTERESTS = 1000;
-  final long CREDIT_MAX_LIMIT = 0;
+  static final long CREDIT_INTERESTS = 1000;
+  static final long CREDIT_MAX_LIMIT = 0;
 
 }
--- Labs/Lab20/src/main/java/BankSystem/Model/Accounts/PensionAccount.java
+++ Labs/Lab20/src/main/java/BankSystem/Model/Accounts/PensionAccount.java
@@ -1,10 +1,11 @@
 package BankSystem.Model.Accounts;
 
-import static BankSystem.Model.Accounts.AccountConst.MIN_AGE;
+import static BankSystem.Model.Accounts.AccountConst.*;
 
 /**
  * Pension Account
  */
 public class PensionAccount extends SavingAccount {
-  private long minAge = MIN_AGE;
+  private long minAge = PENSION_MIN_AGE;
+  private long minBalance = PENSION_MIN_BALANCE;
 }
