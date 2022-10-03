8a93c357af2c941d7205c831b139588b92de730e
mononerv <me@mononerv.dev>
2022-10-03 23:47:45 +0200

    L10: Task screen runner

--- 2022/L10/Runner.java
+++ 2022/L10/Runner.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
+
 public class Runner {
     public static void main(String[] args) {
-        Task tasks[13];
         tasks[0]  = new LinearSearch();
         tasks[1]  = new FindMaximum();
         tasks[2]  = new FindMinimum();
@@ -15,6 +16,44 @@ public class Runner {
         tasks[11] = new DentistClinic();
         tasks[12] = new DentistExtend();
 
+        while (true) {
+            print_tasks();
+            var value = read_int();
+            if (value == -1) break;
+            if (value == Integer.MIN_VALUE || value < 0 || value > tasks.length - 1) {
+                System.out.println("Invalid input. Try again.");
+                continue;
+            }
+
+            System.out.println();
+            System.out.println("Running task: " + tasks[value].str());
+            tasks[value].run();
+            System.out.println();
+        }
     }
+
+    private static void print_tasks() {
+        System.out.println("Task list");
+        System.out.println("--------------------------------");
+        for (int i = 0; i < 13; ++i) {
+            if (i < 10) System.out.print(" ");
+            System.out.print(i);
+            System.out.print(". ");
+            System.out.println(tasks[i].str());
+        }
+    }
+
+    private static int read_int() {
+        System.out.print("Select task. Enter -1 to quit: ");
+        var text = input.nextLine();
+        try {
+            return Integer.parseInt(text);
+        } catch (Exception e) {
+            return Integer.MIN_VALUE;
+        }
+    }
+
+    private static Scanner input = new Scanner(System.in);
+    private static Task tasks[] = new Task[13];
 }
 
