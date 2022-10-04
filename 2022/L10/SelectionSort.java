5b0aed52d8e1e051047a6f923e7728b3f897ff30
mononerv <me@mononerv.dev>
2022-10-04 02:34:01 +0200

    L10: Task08. Selection sort

--- 2022/L10/SelectionSort.java
+++ 2022/L10/SelectionSort.java
@@ -7,5 +7,31 @@ public class SelectionSort extends Task {
     @Override
     public void run() {
         int[] array = {32, 27, 64, 18, 95, 14, 90, 70, 60, 37};
+        print_array(array);
+
+        for (int i = 0; i < array.length; ++i) {
+            int min_value_index = -1;
+            for (int j = i; j < array.length; ++j) {
+                if (min_value_index == -1 || array[j] < array[min_value_index])
+                    min_value_index = j;
+            }
+
+            if (min_value_index != -1) {
+                final var tmp = array[i];
+                array[i]      = array[min_value_index];
+                array[min_value_index] = tmp;
+            }
+        }
+
+        print_array(array);
+    }
+
+    private void print_array(int[] arr) {
+        System.out.print("{");
+        for (int i = 0; i < arr.length; ++i) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) System.out.print(",");
+            else System.out.println("}");
+        }
     }
 }
